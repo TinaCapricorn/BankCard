03622b809a93df7354e6c552678df936872c3a8a
Klein Kristina <klein281293@gmail.com>
2022-02-28 21:09:23 +0700

    Fix1

--- src/test/java/BankCardTest.java
+++ src/test/java/BankCardTest.java
@@ -1,8 +1,12 @@
 import com.codeborne.selenide.Condition;
+import org.apache.hc.core5.util.Asserts;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.lang.module.Configuration;
 import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
@@ -10,18 +14,22 @@ import static com.codeborne.selenide.Selenide.open;
 
 public class BankCardTest {
 
+    String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     public void shouldPass(){
         open("http://localhost:9999");
         $("[data-test-id=city] input").setValue("Новосибирск");
-        $("[data-test-id=date] input").click();
-        $("[data-step='1']").click();
-        $(withText("15")).click();
+        String planningDate = generateDate(11);
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id=name] input").setValue("Василий Пупкин");
         $("[data-test-id=phone] input").setValue("+79112223344");
         $("[data-test-id=agreement]").click();
         $(withText("Забронировать")).click();
-        $(withText("Успешно")).shouldBe(Condition.appear, Duration.ofSeconds(20));
+        $("[class='notification__content']").shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(20));
     }
 
 }
